b929ce998d92717b8a9bf7119c2ad1e3aed315b4
Arjun Bhandari <arjunbhandari3773@gmail.com>
2020-12-15 00:10:22 +0545

    lab2 added

---
+++ lab2/sort_algorithm.py
@@ -0,0 +1,55 @@
+def mergeSort(data, p, r):
+    if p < r:
+        q = int((p+r-1)/2)
+
+        mergeSort(data, p, q)
+        mergeSort(data, q + 1, r)
+        merge(data, p, q, r)
+
+def merge(data, p, q, r):
+    n1 = q - p + 1
+    n2 = r - q
+
+    L = [0] * (n1)
+    R = [0] * (n2)
+
+    for i in range(n1):
+        L[i] = data[p + i]
+    for j in range(n2):
+        R[j] = data[q + j + 1]
+
+    i = 0
+    j = 0
+    k = p
+
+    while (i < n1 and j < n2):
+        if (L[i] <= R[j]):
+            data[k] = L[i]
+            i += 1
+        else:
+            data[k] = R[j]
+            j += 1
+        k += 1
+
+    while (i < n1):
+        data[k] = L[i]
+        i += 1
+        k += 1
+
+    while (j < n2):
+        data[k] = R[j]
+        j += 1
+        k += 1
+
+def insertionSort (data):
+    length = len(data)
+
+    for i in range(1, length):
+        key = data[i]
+        j = i - 1
+
+        while ( (j+1) > 0 and data[j] > key):
+            data[j+1] = data[j]
+            j = j - 1
+
+        data[j+1] = key
